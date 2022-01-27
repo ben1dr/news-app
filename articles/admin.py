810e1a8ec6a20d7539556607b154837bdc1dcead
ben <benaymin.derakhshani@gmail.com>
2022-01-27 13:45:08 +0330

    inital commit

---
+++ articles/admin.py
@@ -0,0 +1,19 @@
+from pyexpat import model
+from statistics import mode
+from django.contrib import admin
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra=0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+    
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
